00d14a13569a6b42e399887b772050f282ae50d2
wildfireOfMine <iamtheanguera@gmail.com>
2025-01-17 23:32:29 +0100

    Finished adding an UI to RPS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
-function playGame(){
-    var humanScore = 0;
-    var computerScore = 0; 
-    for (let i = 0; i<5; i++) {
+
+const rock = document.querySelector("#rock");
+const papers = document.querySelector("#papers");
+const scissors = document.querySelector("#scissors");
+const result = document.querySelector("#result");
+const humanScoreDiv = document.querySelector("#humanScore");
+const computerScoreDiv = document.querySelector("#computerScore");
+const ranking = document.querySelector("#ranking");
+var humanScore = 0;
+var computerScore = 0;
+humanScoreDiv.innerHTML = humanScore;
+computerScoreDiv.innerHTML = computerScore;
+
+
+rock.addEventListener("click", e=>{
+    const score = playRound("rock", getComputerChoice());
+    result.innerHTML = "";
+    result.append(score);
+    humanScoreDiv.innerHTML = humanScore;
+    computerScoreDiv.innerHTML = computerScore;
+    if (humanScore === 5 || computerScore === 5) {
+        winnerFunction();
+    }
+});
+
+papers.addEventListener("click", e=>{
+    const score = playRound("papers", getComputerChoice());
+    result.innerHTML = "";
+    result.append(score);
+    humanScoreDiv.innerHTML = humanScore;
+    computerScoreDiv.innerHTML = computerScore;
+    if (humanScore === 5 || computerScore === 5) {
+        winnerFunction();
+    }
+});
+
+scissors.addEventListener("click", e=>{
+    const score = playRound("scissors", getComputerChoice());
+    result.innerHTML = "";
+    result.append(score);
+    humanScoreDiv.innerHTML = humanScore;
+    computerScoreDiv.innerHTML = computerScore;
+    if (humanScore === 5 || computerScore === 5) {
+        winnerFunction();
+    }
+});
+
+/* function playGame(){
+ 
+    while (humanScore < 5 && computerScore < 5) {
         const verdict = playRound(getHumanChoice(), getComputerChoice());
         if (verdict === 0) {
             humanScore++;
@@ -13,26 +59,23 @@ function playGame(){
         console.log(`SCORES: ${humanScore} - ${computerScore}`);
     }
     
-}
-
-playGame();
+} */
 
 function playRound(humanChoice, computerChoice) {
     const humanChoiceCS = humanChoice.toLowerCase();
-    console.log(humanChoiceCS);
-    console.log(computerChoice);
-    let verdict = 0;
+    let verdict = "";
     if ((humanChoiceCS === "scissors" && computerChoice == "papers") || 
     (humanChoiceCS === "papers" && computerChoice == "rock") ||
     (humanChoiceCS === "rock" && computerChoice == "scissors")) {
-        console.log(`YOU WIN! YOUR CHOICE: ${humanChoiceCS}. COMPUTER'S CHOICE: ${computerChoice}`);
+        verdict = (`YOU WIN! YOUR CHOICE: ${humanChoiceCS}. COMPUTER'S CHOICE: ${computerChoice}`);
+        humanScore++;
     } else if ((computerChoice === "scissors" && humanChoiceCS == "papers") || 
     (computerChoice === "papers" && humanChoiceCS == "rock") ||
     (computerChoice === "rock" && humanChoiceCS == "scissors")) {
-        verdict = 1;
-        console.log(`YOU LOSE! YOUR CHOICE: ${humanChoiceCS}. COMPUTER'S CHOICE: ${computerChoice}`);
+        verdict = (`YOU LOSE! YOUR CHOICE: ${humanChoiceCS}. COMPUTER'S CHOICE: ${computerChoice}`);
+        computerScore++;
     } else {
-        verdict = 2;
+        verdict = "TIE";
     }
 
     return verdict;
@@ -53,8 +96,23 @@ function getComputerChoice(){
     return choice;
 }
 
-function getHumanChoice(){
+function winnerFunction(){
+    if (humanScore == 5) {
+        result.innerHTML += "<br> <strong>The human has won</strong>";
+    } else if (computerScore == 5) {
+        result.innerHTML += "<br> <strong>The computer has won</strong>";
+    }
+    const winner = document.createElement("li");
+    winner.innerText = `Human = ${humanScore} - Computer = ${computerScore}`;
+    ranking.appendChild(winner);
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreDiv.innerHTML = humanScore;
+    computerScoreDiv.innerHTML = computerScore;
+}
+
+/* function getHumanChoice(){
     const getChoice = prompt("Choose your weapon");
 
     return getChoice;
-}
+} */
